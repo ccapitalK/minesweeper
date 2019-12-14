9d3a4c5b5234e2f084b7e101759277be5ab8f2fa
Sahan Fernando <sahan.h.fernando@gmail.com>
2019-12-14 20:52:15 +1100

    Added window resize handling for mouse click position

--- main.cc
+++ main.cc
@@ -32,15 +32,34 @@ int main() {
     sf::Clock frame_timer;
     sf::RectangleShape panel;
 
+    int current_width = width;
+    int current_height = height;
+    auto normalize_mb_event = [&] (auto &mb_ev) {
+        double scale_x = ((double)current_width)/((double)width);
+        double scale_y = ((double)current_height)/((double)height);
+        mb_ev.x = mb_ev.x / scale_x;
+        mb_ev.y = mb_ev.y / scale_y;
+    };
+
     int i=0;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             switch (event.type) {
+            case sf::Event::Resized:
+                current_width = event.size.width;
+                current_height = event.size.height;
+                std::cout <<
+                    current_width << ' ' <<
+                    current_height << ' ' <<
+                    width << ' ' <<
+                    height << '\n';
+                break;
             case sf::Event::Closed:
                 window.close();
                 continue;
             case sf::Event::MouseButtonReleased:
+                normalize_mb_event(event.mouseButton);
                 board.handle_mouse_click(event.mouseButton);
                 break;
             }
